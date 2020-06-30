ca7d94262c2e4338312feb13f3cdef716d55f916
PabloBaiocchi <pablo.b.baiocchi@gmail.com>
2020-06-30 13:04:49 -0300

    spreadsheet auth

---
+++ data.py
@@ -0,0 +1,18 @@
+import gspread 
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+import pandas as pd
+
+def getSpreadsheetAuth(): 
+  auth.authenticate_user()
+  spreadsheetAuth=gspread.authorize(GoogleCredentials.get_application_default())
+  return spreadsheetAuth
+
+def getCashFlowFrame(spreadsheetAuth,sheet_url):
+  sheet=spreadsheetAuth.open_by_url(sheet_url)
+  frame=get_as_dataframe(sheet.worksheet('Sheet1'))
+  frame=frame.dropna(how='all')
+  frame=frame.dropna(axis='columns',how='all')
+  frame['date'] = pd.to_datetime(frame['date'], format='%Y-%m-%d')
+  return frame
